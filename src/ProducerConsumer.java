ab5449e0f749777f0378b676d070899b21574d73
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-18 16:43:24 -0600

    Write a program with one thread (the producer) that produces items (in this case, simple ints). Another thread (the consumer) consumes items. For communacation purposes, both threads have access to a bounded buffer which is basically an array.

---
+++ src/ProducerConsumer.java
@@ -0,0 +1,92 @@
+//to store list of jobs in queue.
+import java.util.LinkedList;
+public class ProducerConsumer {
+    public static void main(String[] args)
+            throws InterruptedException
+    {
+        final PC pc = new PC();
+
+        // Create producer thread
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                try {
+                    pc.produce();
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Create consumer thread
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                try {
+                    pc.consume();
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Start both threads
+        t1.start();
+        t2.start();
+
+        // t1 finishes before t2
+        t1.join();
+        t2.join();
+    }
+
+
+    public static class PC {
+
+        // Create a list shared by producer and consumer
+        LinkedList<Integer> list = new LinkedList<>();
+        int capacity = 2;
+
+        // Function called by producer thread
+        public void produce() throws InterruptedException
+        {
+            int value = 0;
+            while (true) {
+                synchronized (this)
+                {
+                    while (list.size() == capacity)
+                        wait();
+
+                    System.out.println("Producer produced-"
+                            + value);
+                    list.add(value++);
+                    notify();
+
+                    Thread.sleep(1000);
+                }
+            }
+        }
+
+        // Function called by consumer thread
+        public void consume() throws InterruptedException
+        {
+            while (true) {
+                synchronized (this)
+                {
+                    while (list.size() == 0)
+                        wait();
+                    int val = list.removeFirst();
+
+                    System.out.println("Consumer consumed-"
+                            + val);
+                    notify();
+
+                    Thread.sleep(1000);
+                }
+            }
+        }
+    }
+}
