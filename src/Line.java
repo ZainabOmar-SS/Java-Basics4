7d06ff715ae7922b9327d28b75a595502e893b18
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-20 16:40:07 -0600

    create Line class with 3 methods and LineTest test

--- src/Line.java
+++ src/Line.java
@@ -1,2 +1,23 @@
 public class Line {
+    public double getSlope(double x1, double y1, double x2, double y2){
+        return (y2 - y1)/(x2 - x1);
+    }
+
+    public double getDistance(double x1, double y1, double x2, double y2){
+        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    }
+
+    public void parallelTo(double x1, double y1, double x2, double y2){
+        if(x1 * y2 == x2 * y1){
+            System.out.println("parallel");
+        }
+        else {
+            System.out.println("not parallel");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/LineTest.java
+++ src/LineTest.java
@@ -2,4 +2,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LineTest {
 
+    @org.junit.jupiter.api.Test
+    void getSlope() {
+        (x2 - x1) != 0;
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDistance() {
+        Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)) != 0;
+    }
+
+    @org.junit.jupiter.api.Test
+    void parallelTo() {
+    }
 }
